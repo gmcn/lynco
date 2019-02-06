3822341579af16485ebb3f5d21e3936caff1891f
Web Dev <webdev@Webs-MBP.lan>
2019-02-06 14:32:46 +0000

    support for second column of content in about bottom section

--- wp-content/themes/starting-theme/inc/page-about/bottom-content.php
+++ wp-content/themes/starting-theme/inc/page-about/bottom-content.php
@@ -4,6 +4,10 @@ $section_3_heading = get_field('section_3_heading');
 $section_3_grid_layout = get_field('section_3_grid_layout');
 $section_3_copy = get_field('section_3_copy');
 
+$section_3_heading_right = get_field('section_3_heading_right');
+$section_3_right_content = get_field('section_3_right_content');
+$section_3_right_video = get_field('section_3_right_video');
+
  ?>
 
 
@@ -20,8 +24,26 @@ $section_3_copy = get_field('section_3_copy');
 
         </div>
 
+        <div class="col-md-4 col-md-offset-3 section3">
+
+        <h3><?php echo $section_3_heading_right ?></h3>
+
+        <?php if($section_3_right_content) : ?>
+          <?php echo $section_3_right_content ?>
+        <?php endif; ?>
+
+        <?php if($section_3_right_video) : ?>
+
+          <iframe width="100%" height="315" src="https://www.youtube.com/embed/<?php echo $section_3_right_video ?>" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <?php endif; ?>
+
+
+
+        </div>
+
 
       <?php elseif($section_3_grid_layout == 'yes') : ?>
+
         <?php if( have_rows('section_3_grid_copy') ): ?>
 
           <div class="col-md-3 col-md-offset-1 section3_grid">
