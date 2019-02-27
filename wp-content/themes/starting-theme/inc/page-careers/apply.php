5a791d6aec70105540d34b0e24c2ca9bf4efb00d
Web Dev <webdev@Webs-MBP.lan>
2019-02-27 16:47:48 +0000

    using two different contact forms, depending on what is role

--- wp-content/themes/starting-theme/inc/page-careers/apply.php
+++ wp-content/themes/starting-theme/inc/page-careers/apply.php
@@ -18,14 +18,18 @@ $application_form = get_field('application_form');
         To apply for this job click the link below to download an application form. Complete and upload via the form below before the closing date.
       </p>
       <p>
-        Alternatively send your application to <a href="mailto:careers@lyncofacades.com">careers@lyncofacades.com</a>
+        Alternatively send your application to <a href="mailto:careers@lyncoltd.com">careers@lyncoltd.com</a>
       </p>
       <?php if($application_form) : ?>
         <a class="download-app" href="<?php echo $application_form ?>" download>Download Application</a>
       <?php endif; ?>
     </div><!-- /.col-md-4 -->
     <div class="col-md-8 col-md-offset-1">
-      <?php echo do_shortcode('[contact-form-7 id="214" title="Career Application"]'); ?>
+      <?php if (is_page(365)): ?>
+        <?php echo do_shortcode('[contact-form-7 id="727" title="Career Application Sub Contractor"]'); ?>
+      <?php else : ?>
+        <?php echo do_shortcode('[contact-form-7 id="214" title="Career Application"]'); ?>
+      <?php endif; ?>
     </div><!-- /.col-md-8 -->
   </div><!-- /.row -->
 </div><!-- /.container-fluid apply -->
